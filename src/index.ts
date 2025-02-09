ed6b02097a536e03db0cfa084fe4ae8a9fbdf598
elijahawork <64611104+elijahawork@users.noreply.github.com>
2025-02-08 23:10:38 -0600

    added some tests and added run-time type checking on promise return

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,76 @@
+import typia from 'typia';
+import { Lesson } from './LingQ';
+
 // index.ts
 type LanguageCode = string; // expand later to be just those languages that are supported
+type LessonStatus = string; // expand later to be just those statuses that are supported
+type CreateLessonParams = {
+    description: string;
+    groups: unknown[];
+    hasPrice: boolean;
+    isProtected: boolean;
+    isHidden: boolean;
+    language: LanguageCode;
+    status: LessonStatus;
+    tags: unknown[];
+    text: string;
+    title: string;
+    translations: unknown[];
+    notes: string;
+    save: boolean;
+};
 
 export default class LingqAPI {
-	private languageCode: LanguageCode;
-	private lessonCode: number;
-	private csrfToken: string;
-	private sessionId: string;
+    private languageCode: LanguageCode;
+    private lessonCode: number;
+    private csrfToken: string;
+    private sessionId: string;
 
-	private static VERSION = 'Web/5.3.38'; // update this or you will be rejected
-	private commonHeaders: {
-		'Accept': 	 string,
-		'Priority':  string,
-		'Sec-CH-UA': string,
-		'Sec-CH-UA-Mobile': string,
-		'Sec-CH-UA-Platform': string,
-		'Sec-Fetch-Dest': string,
-		'Sec-Fetch-Mode': string,
-		'Sec-Fetch-Site': string,
-		'X-Lingq-App': typeof LingqAPI.VERSION,
-		'Referer': string;
-		'Referrer-Policy': string;
-		'Cookie'?: string
-		'Content-Type'?: string,
-		'X-CSRFToken'?: string,
-	};  // the headers that every request uses.
-	
+    private static VERSION = 'Web/5.3.38'; // update this or you will be rejected
+    private commonHeaders: {
+        'Accept': string;
+        'Priority': string;
+        'Sec-CH-UA': string;
+        'Sec-CH-UA-Mobile': string;
+        'Sec-CH-UA-Platform': string;
+        'Sec-Fetch-Dest': string;
+        'Sec-Fetch-Mode': string;
+        'Sec-Fetch-Site': string;
+        'X-Lingq-App': typeof LingqAPI.VERSION;
+        'Referer': string;
+        'Referrer-Policy': string;
+        'Cookie'?: string;
+        'Content-Type'?: string;
+        'X-CSRFToken'?: string;
+    }; // the headers that every request uses.
 
-
-    constructor(languageCode: LanguageCode, lessonCode: number, csrfToken: string, sessionId: string) {
+    constructor(
+        languageCode: LanguageCode,
+        lessonCode: number,
+        csrfToken: string,
+        sessionId: string
+    ) {
         this.languageCode = languageCode;
         this.lessonCode = lessonCode;
         this.csrfToken = csrfToken;
         this.sessionId = sessionId;
 
-		this.commonHeaders = 	{
-			'Accept': 'application/json',
-			'Priority': 'u=1, i',
-			'Sec-CH-UA': '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
-			'Sec-CH-UA-Mobile': '?0',
-			'Sec-CH-UA-Platform': '"macOS"',
-			'Sec-Fetch-Dest': 'empty',
-			'Sec-Fetch-Mode': 'cors',
-			'Sec-Fetch-Site': 'same-origin',
-			'X-Lingq-App': LingqAPI.VERSION,
-			'Referer': `https://www.lingq.com/en/learn/${this.languageCode}/web/editor/${this.lessonCode}`,
-			'Referrer-Policy': 'strict-origin-when-cross-origin',
-		};	
+        this.commonHeaders = {
+            'Accept': 'application/json',
+            'Priority': 'u=1, i',
+            'Sec-CH-UA': '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+            'Sec-CH-UA-Mobile': '?0',
+            'Sec-CH-UA-Platform': '"macOS"',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-origin',
+            'X-Lingq-App': LingqAPI.VERSION,
+            'Referer': `https://www.lingq.com/en/learn/${this.languageCode}/web/editor/${this.lessonCode}`,
+            'Referrer-Policy': 'strict-origin-when-cross-origin',
+        };
     }
 
-    buildHeaders({ isPost = false, includeCsrf = false }) {
+    private buildHeaders({ isPost = false, includeCsrf = false }) {
         const headers = { ...this.commonHeaders };
         headers['Cookie'] = `csrftoken=${this.csrfToken}; wwwlingqcomsa=${this.sessionId};`;
 
@@ -63,30 +85,66 @@ export default class LingqAPI {
         return headers;
     }
 
-    async getLesson() {
+    async getLesson(): Promise<Lesson> {
         const response = await fetch(`${this._baseURL()}/editor/`, {
             method: 'GET',
             headers: this.buildHeaders({ isPost: false }),
         });
-        return response.json();
+
+        const lesson: Lesson = await response.json();
+
+        typia.assert<Lesson>(lesson); // Throws an error if the response is invalid
+
+        return lesson;
     }
 
-    _baseURL () {
+    /**
+     *
+     * @param lessonParams
+     * @returns nothing
+     * NOTE: This switches the the API to the newly created lesson.
+     */
+    async createLesson(lessonParams: CreateLessonParams): Promise<undefined> {
+        // returns nothing
+        const response = await fetch(
+            `https://www.lingq.com/api/v3/${this.lessonCode}/lessons/import/`,
+            {
+                method: 'POST',
+                headers: this.buildHeaders({ isPost: true, includeCsrf: true }),
+                body: JSON.stringify(lessonParams),
+            }
+        );
+
+        if (!response.ok) {
+            throw new Error(`Failed to create lesson: ${response.statusText}`);
+        }
+
+        return;
+    }
+    async deleteLesson(lessonId: number) {
+        fetch(`https://www.lingq.com/api/v2/contexts/7198304/lesson/`, { // the context indicates which webpack function it's using I think
+            headers: this.buildHeaders({ isPost: true, includeCsrf: true }),
+            body: JSON.stringify({ lesson: lessonId }),
+            method: 'DELETE',
+        });
+    }
+
+    _baseURL() {
         return `https://www.lingq.com/api/v3/${this.languageCode}/lessons/${this.lessonCode}`;
     }
     _sentencesURL() {
-        return `${this._baseURL()}/sentences/`
+        return `${this._baseURL()}/sentences/`;
     }
-    async _postSentences(body: any) {
+    private async _postSentences(body: any) {
         const sentences = this._sentencesURL();
         const init = {
             method: 'POST',
             headers: this.buildHeaders({ isPost: true, includeCsrf: true }),
             body: JSON.stringify(body),
         };
-        
+
         const response = await fetch(sentences, init);
-        
+
         return response;
     }
     async updateSentenceTimestamp(index: number, timestamp: [number, number]) {
@@ -97,12 +155,12 @@ export default class LingqAPI {
             lone: false,
         });
     }
-	/**
-	 * 
-	 * @param index starting at 1
-	 * @param text 
-	 * @returns 
-	 */
+    /**
+     *
+     * @param index starting at 1
+     * @param text
+     * @returns
+     */
     async updateSentenceText(index: number, text: string) {
         return await this._postSentences({
             action: 'update',
@@ -112,13 +170,13 @@ export default class LingqAPI {
         });
     }
 
-	/**
-	 * 
-	 * @param index the index you want the new sentence to have, displacing the current one forwards
-	 * @param text 
-	 * @param after if you want the new sentence to be after the current one. The default is false.
-	 * @returns 
-	 */
+    /**
+     *
+     * @param index the index you want the new sentence to have, displacing the current one forwards
+     * @param text
+     * @param after if you want the new sentence to be after the current one. The default is false.
+     * @returns
+     */
     async createSentence(index: number, text: string, after = false) {
         // after=false means that the new sentence will be @ index
         return await this._postSentences({
@@ -134,7 +192,7 @@ export default class LingqAPI {
         return await this._postSentences({
             index: index,
             action: 'delete',
-        }) // the api returns no body for delete requests.
+        }); // the api returns no body for delete requests.
     }
     async breakSentence(index: number) {
         return await this._postSentences({
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,15 +1,19 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, beforeAll } from "vitest";
 import LingQ from '../src/index';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
+// Load config
 const config = {
 	langCode: process.env.LANG_CODE,
 	lessonCode: process.env.LESSON_CODE,
 	csrfToken: process.env.CSRF_TOKEN,
 	sessionId: process.env.SESSION_ID
-}
+};
+
+// Ensure all required env variables are set
+const isConfigLoaded = Object.values(config).every(value => value);
 
 describe('Check env/config', () => {
 	it('should have a langCode', () => {
@@ -26,3 +30,17 @@ describe('Check env/config', () => {
 	});
 });
 
+// Only run tests if config is valid
+describe('LingQ', () => {
+	beforeAll(() => {
+		if (!isConfigLoaded) {
+			throw new Error("Missing environment variables. Aborting tests.");
+		}
+	});
+
+	it('should function', async () => {
+		const lingQ = new LingQ(config.langCode!, parseInt(config.lessonCode!), config.csrfToken!, config.sessionId!);
+		const res = await lingQ.getLesson();
+		expect(res).toBeDefined();
+	});
+});
